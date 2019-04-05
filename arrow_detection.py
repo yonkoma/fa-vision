997e7ab6b7c5bdae2a5e5fd25b16d3da01fb455d
rafer45 <ataque992@hotmail.com>
2019-04-05 01:24:42 -0400

    Add base and head mask

--- arrow_detection.py
+++ arrow_detection.py
@@ -1,8 +1,13 @@
 
 import cv2
 import numpy as np
+import general_pipeline as gp
 
 import color_util as cu
+
+def show(name, img):
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.imshow(name, img)
 
 # arrowbases = array of positions of arrow starts
 # arrowheads = array of positions of arrow_ends
@@ -10,7 +15,12 @@ import color_util as cu
 arrowbase_hue_range = cu.grn_hue_range
 arrowhead_hue_range = cu.red_hue_range
 
-# TODO: Function that takes an img and returns the centroids of the bases
+def base_mask(hsv_img):
+    return cu.hue_mask(arrowbase_hue_range, 50, hsv_img)
+
+def head_mask(hsv_img):
+    return cu.hue_mask(arrowhead_hue_range, 50, hsv_img)
+
 # TODO: Function that takes an img and returns the centroids of the heads
 # TODO: Function that takes an array of base centroids, an array of head centroids,
 #   and the image, then returns an array of [base, head] pairs
